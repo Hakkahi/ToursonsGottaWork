d3fbd3b44472dbd3a07ddbae88ccc98483a25ccb
Hakkahi <antoinegeorges00@gmail.com>
2019-05-13 14:29:48 +0200

    graphic update

--- content-script/main.js
+++ content-script/main.js
@@ -1,33 +1,114 @@
 var browser = browser || chrome
 
-function onGot(list) {
-    if(list.domainList.includes(location.host)){
-        document.head = `
-            <head>
-                <meta charset="utf-8">
-            </head>
-        `
-        document.body.innerHTML = `
-            <div>
-                <p>COURAGE TOURSONNE</p>
-            </div>
-        `
+async function onGot(list) {
+    if (Object.keys(list).length === 0 && list.constructor === Object) {
+        list.domainList = [];
+    }
+    if (list.domainList.includes(location.host)) {
+        html = await buildDocumentContent()
+        document.open()
+        document.write(html)
+        document.close()
     }
 }
 
 // Getting the activated variable 
-browser.storage.sync.get('activated', function(value){
-    if(Object.keys(value).length === 0 && value.constructor === Object){
-        browser.storage.sync.set({
+browser.storage.sync.get('activated', function(value) {
+    // If there is no values in the storage we consider that the extension is on
+    if (Object.keys(value).length === 0 && value.constructor === Object) {
+        browser.storage.sync.get({
             activated: true
         });
-        
+
         var getting = browser.storage.sync.get("domainList", onGot);
-    }
-    else{
-        if(value.activated){
+    } else {
+        if (value.activated) {
             var getting = browser.storage.sync.get("domainList", onGot);
         }
     }
 });
 
+
+async function buildDocumentContent() {
+    let promise = new Promise(function(resolve, reject) {
+        // Waiting for all the ressources to be fetched
+        Promise.all([fetchExtensionTextResource("content-script/ressources/customContent.html"), fetchExtensionTextResource("content-script/ressources/customContent.css"), fetchExtensionTextResource("content-script/ressources/customContent.js")]).then(async(values) => {
+            let htmlContentElement = document.createElement("div");
+            htmlContentElement.innerHTML = values[0];
+
+            let css = document.createElement("style");
+            css.type = 'text/css'
+            css.innerHTML = values[1]
+            try {
+                let imageTmpURL = await fetchExtensionBlobResource("content-script/ressources/images/background.webp")
+                css.innerHTML = css.innerHTML + `body {background-image: url(${imageTmpURL})}`
+            } catch (error) {
+                reject(error)
+            }
+            htmlContentElement.appendChild(css)
+
+            let js = document.createElement("script");
+            js.innerHTML = values[2]
+            htmlContentElement.appendChild(js)
+
+            resolve(htmlContentElement.innerHTML)
+        }).catch(function(error) {
+            reject(error)
+        })
+    })
+    try {
+        let html = await promise
+        return html
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        return ''
+    }
+
+
+}
+
+
+function fetchExtensionTextResource(resourcePath) {
+    let promise = new Promise(function(resolve, reject) {
+        // Fetching css content
+        fetch(browser.extension.getURL(resourcePath)).then((response) => {
+                if (response.ok) {
+                    response.text().then(function(content) {
+                        resolve(content)
+                    })
+                } else {
+                    console.log('Bad network answer');
+                    reject('Bad network answer')
+                }
+            })
+            .catch(function(error) {
+                console.log(`Got a problem with the fetch operation ${error.message}`);
+                reject(error)
+            })
+    })
+
+    return promise;
+}
+
+function fetchExtensionBlobResource(resourcePath) {
+    let promise = new Promise(function(resolve, reject) {
+        // Fetching css content
+        fetch(browser.extension.getURL(resourcePath)).then((response) => {
+                if (response.ok) {
+                    response.blob().then(function(content) {
+                        let url = URL.createObjectURL(content)
+                        resolve(url)
+                    })
+                } else {
+                    console.log('Bad network answer');
+                    reject('Bad network answer')
+                }
+            })
+            .catch(function(error) {
+                console.log(`Got a problem with the fetch operation ${error.message}`);
+                reject(error)
+            })
+    })
+
+    return promise;
+}
--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,24 +1,25 @@
 var browser = browser || chrome
 
 var toggle = document.querySelector('input[type="checkbox"]');
-browser.storage.sync.get('activated', function(value){
-    if(value.activated){
+browser.storage.sync.get('activated', function(value) {
+    if (value.activated) {
         toggle.checked = true
-    }
-    else{
-        toggle.checked = false        
+    } else {
+        toggle.checked = false
     }
 })
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    
-    toggle.addEventListener('change', function () {
+document.addEventListener('DOMContentLoaded', function() {
+
+    toggle.addEventListener('change', function() {
         if (toggle.checked) {
+            console.log("ON");
             browser.storage.sync.set({
                 activated: true
             });
         } else {
+            console.log('OFF');
             browser.storage.sync.set({
                 activated: false
             });
